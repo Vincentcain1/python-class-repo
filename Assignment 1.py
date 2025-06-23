8efc05f5be21ce63dd6172d04003a55349f9cd29
Vincentcain1 <vincentcain237@gmail.com>
2025-06-22 17:56:33 -0700

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,24 @@
+Python 3.13.3 (v3.13.3:6280bb54784, Apr  8 2025, 10:47:54) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> "test"
+'test'
+>>> myname = "vincent"
+>>> myname = vincent
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    myname = vincent
+NameError: name 'vincent' is not defined
+>>> 3=7
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+>>> 7*2
+14
+>>> 10/5
+2.0
+>>> 9==9
+True
+>>> 14-2==12
+True
+>>> 4738!=17
+True
+>>> print("completed by, Vincent Cain")
+completed by, Vincent Cain
